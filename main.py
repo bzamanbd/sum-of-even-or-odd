261997888e79669153776ed8a0744c8aeed4878b
bzamanbd <riponzamanbd@gmail.com>
2024-05-17 08:50:35 +0600

    sum of odds or evens creator

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def sum_of_even(nums:list[int]):
+def sum_of_even_odd(nums:list):
     total:int = 0
     for num in nums:
-        if (num % 2 ) ==0:
+        # if (num % 2) == 0:
+        if (num % 2) != 0:
             total += num
-    print(f"Total is {total}") 
+    # print(f"The total of evens is {total}")
+    print(f"The total of odds is {total}")
 
-sum_of_even([1,2,3,4,5,6])
-
+sum_of_even_odd([1,2,3,4,5,6,7,8,9])
